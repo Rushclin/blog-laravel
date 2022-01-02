3b4ee1afb0d599b38c253ceeb1b46828ccef4c86
Rushclin <takamrushclin@gmail.com>
2022-01-02 19:26:30 +0100

    Creation des migrations et des seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,9 @@
 
 namespace Database\Seeders;
 
+use App\Models\Category;
+use App\Models\Post;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,5 +17,15 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+        Category::factory(5)->create();
+
+        $categories = Category::factory(5)->create();
+
+        User::factory(10)->create()->each(function($user) use ($categories) {
+            Post::factory(rand(1, 3))->create([
+                'user_id' => $user->id,
+                'category_id' => ($categories->random(1)->first())->id
+            ]);
+        });
     }
 }
